1b9dd6286bdf7504d5f90452d7a09119e96cdac1
Oskar Blomberg <blomberg.oskar@gmail.com>
2024-06-16 17:24:53 +0200

    Lvl4: Add homeworld rendering for character search

--- script.js
+++ script.js
@@ -5,87 +5,107 @@ const planetsSearchField = document.getElementById("planets-search-field");
 const planetsSearchBtn = document.getElementById("planets-search-btn");
 const planetsResults = document.querySelector("#planets-results");
 
-peopleSearchBtn.addEventListener("click", findPerson);
-planetsSearchBtn.addEventListener("click", findplanets);
+peopleSearchBtn.addEventListener("click", async () => {
+  if (peopleSearchField.value.trim()) {
+    const cArray = await findPerson();
+    render(cArray, peopleResults);
+  } else {
+    peopleResults.innerHTML = "<p>No results found.</p>";
+  }
+});
+
+planetsSearchBtn.addEventListener("click", () => {
+  if (planetsSearchField.value.trim()) {
+    findPlanets(
+      "https://swapi.dev/api/planets/?search=",
+      planetsSearchField.value
+    );
+  }
+});
 
 async function findPerson() {
-  if (peopleSearchField.value.trim()) {
-    peopleResults.innerHTML = "";
-    const baseUrl = "https://swapi.dev/api/people/?search=";
-    const queryStringName = peopleSearchField.value;
-    const response = await fetch(baseUrl + queryStringName);
-    peopleSearchField.value = "";
-    const data = await response.json();
+  peopleResults.innerHTML = "";
+  const baseUrl = "https://swapi.dev/api/people/?search=";
+  const queryStringName = peopleSearchField.value;
+  const response = await fetch(baseUrl + queryStringName);
+  peopleSearchField.value = "";
+  const data = await response.json();
 
-    if (data.results.length > 0) {
-      const character = data.results[0];
-      console.log(character);
+  if (data.results.length > 0) {
+    const character = data.results[0];
+    console.log(character);
 
-      const cName = "Name: " + character.name;
-      console.log(cName);
-      const cBirth = "Birth year: " + character.birth_year;
-      const cGender = "Gender: " + character.gender;
-      const cHeight = "Height: " + character.height;
-      const cHairColor = "Hair colour: " + character.hair_color;
-      const cEyes = "Eye colour: " + character.eye_color;
-      const cSkin = "Skin colour: " + character.skin_color;
+    const cName = "Name: " + character.name;
+    const cBirth = "Birth year: " + character.birth_year;
+    const cGender = "Gender: " + character.gender;
+    const cHeight = "Height: " + character.height;
+    const cHairColor = "Hair colour: " + character.hair_color;
+    const cEyes = "Eye colour: " + character.eye_color;
+    const cSkin = "Skin colour: " + character.skin_color;
+    const homeWorld = await findPlanets(character.homeworld, "");
+    const cHome = "Homeworld: " + homeWorld.name;
 
-      const cArray = [
-        cName,
-        cBirth,
-        cGender,
-        cHeight,
-        cHairColor,
-        cEyes,
-        cSkin,
-      ];
-
-      render(cArray, peopleResults);
-    } else {
-      peopleResults.innerHTML = "<p>No results found.</p>";
-    }
+    const cArray = [
+      cName,
+      cBirth,
+      cGender,
+      cHeight,
+      cHairColor,
+      cEyes,
+      cSkin,
+      cHome,
+    ];
+    return cArray;
   }
 }
 
-async function findplanets() {
-  if (planetsSearchField.value.trim()) {
-    planetsResults.innerHTML = "";
-    const baseUrl = "https://swapi.dev/api/planets/?search=";
-    const queryStringName = planetsSearchField.value;
-    const response = await fetch(baseUrl + queryStringName);
-    planetsSearchField.value = "";
-    const data = await response.json();
+async function findPlanets(base, qString) {
+  planetsResults.innerHTML = "";
+  const baseUrl = base;
+  const queryStringName = qString;
+  const response = await fetch(baseUrl + queryStringName);
+  planetsSearchField.value = "";
+  const data = await response.json();
+  //Om datan länkar direkt till en planet får vi datan
+  let planet = data;
 
+  // men om vi gör en sökning får vi en array (om sökningen ger resultat)
+  if (data.results) {
     if (data.results.length > 0) {
-      const planet = data.results[0];
-      console.log(planet);
-
-      const pName = "Name: " + planet.name;
-      console.log(pName);
-      const pDiameter = "Diameter: " + planet.diameter;
-      const pPop = "Population: " + planet.population;
-      const pClimate = "Climate: " + planet.climate;
-      const pGravity = "Gravity: " + planet.gravity;
-      const pTerrain = "Terrain: " + planet.terrain;
-      const pRotation = "Rotation period: " + planet.rotation_period;
-      const pYear = "Orbital period: " + planet.orbital_period;
-
-      const cArray = [
-        pName,
-        pDiameter,
-        pPop,
-        pClimate,
-        pGravity,
-        pTerrain,
-        pRotation,
-        pYear,
-      ];
-
-      render(cArray, planetsResults);
+      planet = data.results[0];
     } else {
       planetsResults.innerHTML = "<p>No results found.</p>";
     }
   }
+  console.log(planet);
+
+  const planetArray = storePlanet(planet);
+
+  render(planetArray, planetsResults);
+  return planet;
+}
+
+function storePlanet(planet) {
+  const pName = "Name: " + planet.name;
+  const pDiameter = "Diameter: " + planet.diameter;
+  const pPop = "Population: " + planet.population;
+  const pClimate = "Climate: " + planet.climate;
+  const pGravity = "Gravity: " + planet.gravity;
+  const pTerrain = "Terrain: " + planet.terrain;
+  const pRotation = "Rotation period: " + planet.rotation_period;
+  const pYear = "Orbital period: " + planet.orbital_period;
+
+  const cArray = [
+    pName,
+    pDiameter,
+    pPop,
+    pClimate,
+    pGravity,
+    pTerrain,
+    pRotation,
+    pYear,
+  ];
+  return cArray;
 }
 
 function render(array, section) {
@@ -95,49 +115,3 @@ function render(array, section) {
     section.appendChild(newP);
   }
 }
-
-/* Hämta alla saker man vill ha och stoppa dem i en lista/array
-rendera en lista med alla saker, t.ex. "Name: Obi-Wan"
-Kan nog göras via en for loop eller en forEach */
-
-/* 
-inputBtn.addEventListener("click", function () {
-  const inputValue = inputEl.value.trim();
-
-  if (inputValue && isValidURL(inputValue)) {
-    myLeads.push(inputValue);
-    inputEl.value = "";
-    render(myLeads);
-  } else {
-    alert("Please enter a valid URL");
-  }
-});
-
-function render(leads) {
-  ulEl.textContent = "";
-
-  leads.forEach(lead => {
-    const li = document.createElement('li');
-    const a = document.createElement('a');
-    a.href = lead;
-    a.textContent = lead;
-    a.target = '_blank';
-
-    li.appendChild(a);
-    ulEl.appendChild(li);
-  });
-}
-
-function isValidURL(string) {
-  try {
-    new URL(string);
-    return true;
-    } catch (_) {
-    return false;
-   }
-}
-
-inputEl.addEventListener("input", function () {
-  inputBtn.disabled = !inputEl.value.trim();
-});
-*/
